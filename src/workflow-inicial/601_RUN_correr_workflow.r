cda1604aee705f35bf158138fdce2ba347b14f1e
sposti <sebastianpostiglioneua@gmail.com>
2023-07-04 00:48:14 +0000

    auto commit 1       20230704 004813

--- src/workflow-inicial/601_RUN_correr_workflow.r
+++ src/workflow-inicial/601_RUN_correr_workflow.r
@@ -1,4 +1,4 @@
-# Corrida general del workflow
+# Corrida general del workflow SP
 
 options(error = function() {
   traceback(20)
@@ -10,11 +10,14 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023r/src/workflow-inicial/z611_CA_reparar_dataset.r")
-source("~/labo2023r/src/workflow-inicial/z621_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-inicial/z631_FE_historia.r")
-source("~/labo2023r/src/workflow-inicial/z641_TS_training_strategy.r")
+source("~/labo2023r/src/workflow-inicial/611_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-inicial/621_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-inicial/631_FE_historia.r")
+source("~/labo2023r/src/workflow-inicial/641_TS_training_strategy.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023r/src/workflow-inicial/z651_HT_lightgbm.r")
-source("~/labo2023r/src/workflow-inicial/z661_ZZ_final.r")
+source("~/labo2023r/src/workflow-inicial/651_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-inicial/661_ZZ_final.r")
+
+
+#sp ok
