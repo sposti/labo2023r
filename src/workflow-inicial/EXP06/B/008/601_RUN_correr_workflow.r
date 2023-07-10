23ac5818a9f169d5d28255d38a8fb202ab034614
sposti <sebastianpostiglioneua@gmail.com>
2023-07-10 09:18:00 -0300

    exp06 corrida 008 cambio carpeta ok

--- src/workflow-inicial/EXP06/B/008/601_RUN_correr_workflow.r
+++ src/workflow-inicial/EXP06/B/008/601_RUN_correr_workflow.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023r/src/workflow-inicial/EXP03/B/008/611_CA_reparar_dataset.r")
-source("~/labo2023r/src/workflow-inicial/EXP03/B/008/621_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-inicial/EXP03/B/008/631_FE_historia.r")
-source("~/labo2023r/src/workflow-inicial/EXP03/B/008/641_TS_training_strategy.r")
+source("~/labo2023r/src/workflow-inicial/EXP06/B/008/611_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-inicial/EXP06/B/008/621_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-inicial/EXP06/B/008/631_FE_historia.r")
+source("~/labo2023r/src/workflow-inicial/EXP06/B/008/641_TS_training_strategy.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023r/src/workflow-inicial/EXP03/B/008/651_HT_lightgbm.r")
-source("~/labo2023r/src/workflow-inicial/EXP03/B/008/661_ZZ_final.r")
+source("~/labo2023r/src/workflow-inicial/EXP06/B/008/651_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-inicial/EXP06/B/008/661_ZZ_final.r")
