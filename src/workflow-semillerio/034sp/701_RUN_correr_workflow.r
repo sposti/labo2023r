90aec3c01affcbaf50aff522b362cdc4c6d0324d
sposti <sebastianpostiglioneua@gmail.com>
2023-07-28 11:39:54 -0300

    036

--- src/workflow-semillerio/034sp/701_RUN_correr_workflow.r
+++ src/workflow-semillerio/034sp/701_RUN_correr_workflow.r
@@ -8,9 +8,6 @@ options(error = function() {
 
 
 # corrida de cada paso del workflow
-#source("~/labo2023r/src/workflow-semillerio/034sp/711_CA_reparar_dataset.r")
-#source("~/labo2023r/src/workflow-semillerio/034sp/721_DR_corregir_drifting.r")
-#source("~/labo2023r/src/workflow-semillerio/034sp/731_FE_historia.r")
-#source("~/labo2023r/src/workflow-semillerio/034sp/741_TS_training_strategy.r")
-source("~/labo2023r/src/workflow-semillerio/034sp/751_HT_lightgbm.r")
-source("~/labo2023r/src/workflow-semillerio/034sp/771_ZZ_final_semillerio.r")
+
+source("~/labo2023r/src/workflow-semillerio/036sp/751_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-semillerio/036sp/771_ZZ_final_semillerio.r")
