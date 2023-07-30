3aa556d5539715979b071f11f9ca91cd1591e2c6
sposti <sebastianpostiglioneua@gmail.com>
2023-07-30 15:47:03 -0300

    ts 047 ok

--- src/workflow-semillerio/047sp/741_TS_training_strategy.r
+++ src/workflow-semillerio/047sp/741_TS_training_strategy.r
@@ -11,9 +11,9 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "CC7410_039"
+PARAM$experimento <- "CC7410_047"
 
-PARAM$exp_input <- "FE7310_039"
+PARAM$exp_input <- "FE7310_047"
 
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
