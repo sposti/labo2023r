373cfacf49ab56fc25c4eb7283aafc5ca09c30d8
sposti <sebastianpostiglioneua@gmail.com>
2023-07-30 13:19:43 -0300

    047
    ultimo esfuerzo de BS a full 24 p

---
+++ src/workflow-semillerio/047sp/701_RUN_correr_workflow.r
@@ -0,0 +1,16 @@
+# Corrida general del workflow Semillerio
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+source("~/labo2023r/src/workflow-semillerio/047sp/711_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-semillerio/047sp/721_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-semillerio/047sp/731_FE_historia.r")
+source("~/labo2023r/src/workflow-semillerio/047sp/741_TS_training_strategy.r")
+source("~/labo2023r/src/workflow-semillerio/047sp/751_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-semillerio/047sp/771_ZZ_final_semillerio.r")
