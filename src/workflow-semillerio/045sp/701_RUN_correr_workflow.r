f814ae50ef3852ebc9acb601c949cb9821a8f555
sposti <sebastianpostiglioneua@gmail.com>
2023-07-29 21:23:09 -0300

    045 cv de 3 meses

---
+++ src/workflow-semillerio/045sp/701_RUN_correr_workflow.r
@@ -0,0 +1,16 @@
+# Corrida general del workflow Semillerio
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow   TOMO DEL 041_SP
+source("~/labo2023r/src/workflow-semillerio/045sp/711_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-semillerio/045sp/721_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-semillerio/045sp/731_FE_historia.r")
+source("~/labo2023r/src/workflow-semillerio/045sp/741_TS_training_strategy.r")
+source("~/labo2023r/src/workflow-semillerio/045sp/751_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-semillerio/045sp/771_ZZ_final_semillerio.r")
