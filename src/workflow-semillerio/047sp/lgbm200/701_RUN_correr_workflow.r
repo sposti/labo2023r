dd02074ce788f669505a84b97e93c91b3eef0421
sposti <sebastianpostiglioneua@gmail.com>
2023-07-31 19:13:55 -0300

    Update 701_RUN_correr_workflow.r

--- src/workflow-semillerio/047sp/lgbm200/701_RUN_correr_workflow.r
+++ src/workflow-semillerio/047sp/lgbm200/701_RUN_correr_workflow.r
@@ -9,6 +9,6 @@ options(error = function() {
 
 # corrida de cada paso del workflow
 
-source("~/labo2023r/src/workflow-semillerio/047sp/lgbm200/741_TS_training_strategy.r")
+
 source("~/labo2023r/src/workflow-semillerio/047sp/lgbm200/751_HT_lightgbm.r")
 source("~/labo2023r/src/workflow-semillerio/047sp/lgbm200/771_ZZ_final_semillerio.r")
